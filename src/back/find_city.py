89d2a7bf112e3482ddf566452ca3b88c73c073d2
Nexgear75 <a.fgx2004@gmail.com>
2023-11-27 15:39:05 +0100

    :truck: Add find_city function

--- src/back/find_city.py
+++ src/back/find_city.py
@@ -1,9 +1,17 @@
 from parse import parsing
 
 def find_city(city):
+    """This function look for a city and return lat and lng if found"""
     dictionnaire = parsing("src/assets/villes.txt")
     if city in dictionnaire:
-        return dictionnaire[city]['lat'],dictionnaire[city]['lng']
+        return dictionnaire[city]
     else:
-        return "Ville introuvable dans le dictionnaire"
+        raise Exception
+
+def find_city_with_coordonates(lat, lng):
+    dictionnaire = parsing("src/assets/villes.txt")
+    for key in dictionnaire:
+        if dictionnaire[key]["lat"] == lat and dictionnaire[key]["lng"] == lng:
+            return key
+    return {}
 
