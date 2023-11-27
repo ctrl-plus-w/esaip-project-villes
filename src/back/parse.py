38c73fe3cc5eaa35a652af31fbd54b2afb716dfc
Lukas Laudrain <lukas.ldrn@gmail.com>
2023-11-27 14:16:16 +0100

    :truck: Parsing return type is not a dict

--- src/back/parse.py
+++ src/back/parse.py
@@ -1,12 +1,14 @@
 def parsing(path: str):
     with open(path, 'r') as file:
         villes_coords = {}
+
         for line in file:
             data = line.split()
             ville = data[0]
-            coord1 = data[1]
-            coord3 = data[4]
-            villes_coords[ville] = (coord1, coord3)
+
+            lat = data[1]
+            lng = data[4]
+
+            villes_coords[ville] = {"lat": lat, "lng": lng}
 
     return villes_coords
-
