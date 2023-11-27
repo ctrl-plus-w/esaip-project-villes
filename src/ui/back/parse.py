f796e2e33f86e51164e2b1e1630c3d3b578b242d
Nexgear75 <a.fgx2004@gmail.com>
2023-11-27 13:50:20 +0100

    :truck: Adding parse function

---
+++ src/ui/back/parse.py
@@ -0,0 +1,12 @@
+def parsing():
+    with open('villes.txt', 'r') as file:
+        villes_coords = {}
+        for line in file:
+            data = line.split()
+            ville = data[0]
+            coord1 = data[1]
+            coord3 = data[4]
+            villes_coords[ville] = (coord1, coord3)
+
+    return villes_coords
+
